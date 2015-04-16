2ca2d6e6b032fdbf78086e638429ae1dc4678770
gauravgulzar <gaurav.gulzar@gmail.com>
2015-04-16 15:40:31 +0530

    Initial Commit

--- samer08d.cpp
+++ samer08d.cpp
@@ -15,29 +15,31 @@ void lcs(string s1, string s2, int k){
     int tab[m+1][n+1],c[m+1][n+1],res=0;
     for(int i=0; i<=m; ++i){
         for(int j=0; j<=n; ++j){
-            if(i==0 || j==0) tab[i][j] = 0;
+            if(i==0 || j==0) c[i][j] = tab[i][j] = 0;
             else if(s1[i-1] == s2[j-1]){
+                c[i][j] = c[i-1][j-1]+1;
                 //tab[i][j] = tab[i-1][j-1]+1;
-                int c=1;
-                while(i-c>=0 && j-c >= 0 && s1[i-c]==s2[j-c]) ++c;
-                if(c>=k) tab[i][j] = c-1;
-                
+                tab[i][j] = max(tab[i][j-1],tab[i-1][j]);
+                if(c[i][j]>=k) {
+                    for(int l=k; l<=c[i][j]; ++l)
+                        tab[i][j] = max(tab[i][j],tab[i-l][j-l] + l);
+                }
+                //else tab[i][j] = max(tab[i][j-1],tab[i-1][j]);
             }
-            else tab[i][j] = max(tab[i][j-1],tab[i-1][j]);
+            else { tab[i][j] = max(tab[i][j-1],tab[i-1][j]); c[i][j]=0;}
+            if(c[i][j]>=k) res = max(res,tab[i][j]);
         }
     }
-    
-    for(int i=0; i<=m; ++i){
-        for(int j=0; j<=n; ++j) cout << tab[i][j] << "  ";
-        cout << "\n";
-    }
-    
-    cout << tab[m][n] << "\n";
+    cout << res << "\n";
 }
 
 int main(){
     string s1,s2; int k;
-    cin >> k >> s1 >> s2;
-    lcs(s1,s2,k);
+    while(1){
+        cin >> k;
+        if(!k) break;
+        cin >> s1 >> s2;
+        lcs(s1,s2,k);
+    }
     return 0;
 }
